10a39716cc4055042d07baf094544f6ad9be4e04
JairoDuenas <jairoduenas.ing@gmail.com>
2022-07-18 19:44:19 -0500

    Instalando librerías

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
+import BootstrapeVue from 'bootstrap-vue'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapeVue)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
