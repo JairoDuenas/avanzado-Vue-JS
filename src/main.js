447e70e0450e359de24d3b30a169479222fc56d6
JairoDuenas <jairoduenas.ing@gmail.com>
2022-07-19 09:35:55 -0500

    Refactorizando

--- src/main.js
+++ src/main.js
@@ -1,59 +1,24 @@
 import Vue from 'vue'
-import BootstrapeVue from 'bootstrap-vue'
-
-// Font Awesome
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  faSkull,
-  faCrown,
-  faDungeon,
-  faHatWizard,
-  faHammer,
-  faGem
-} from '@fortawesome/free-solid-svg-icons'
-import {
-  faVuejs,
-  faBootstrap,
-  faFontAwesome,
-  faGithub,
-  faBattleNet
-} from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// BootstrapVue
+import './plugins/BootstrapVue'
+// Vue Font-Awesome
+import './plugins/FontAwesone'
 
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
+// CSS global
 import './assets/css/main.styl'
 
-// Añadiendo iconos
-library.add(
-  faSkull,
-  faCrown,
-  faDungeon,
-  faHatWizard,
-  faHammer,
-  faGem,
-  faVuejs,
-  faBootstrap,
-  faFontAwesome,
-  faGithub,
-  faBattleNet
-)
-
-Vue.use(BootstrapeVue)
 Vue.config.productionTip = false
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
   store,
   methods: {
     init () {
-      store.dispatch('oauth/getToken', null, { root: true })
+      store.dispatch('oauth/getToken')
     }
   },
   created () {
