8da0d1d46847633b8d4915320cb7cebfc1855e9a
JairoDuenas <jairoduenas.ing@gmail.com>
2022-07-21 21:00:00 -0500

    Ajustes footer, about, error, season

--- src/components/FooterBar/MadeByFoot.vue
+++ src/components/FooterBar/MadeByFoot.vue
@@ -2,9 +2,7 @@
   <p class="text-muted m-0 pt-1 pl-3 text-left text-sm-right">
     <small>
       Made by
-      <a href="https://twitter.com/baumannzone" target="_blank"
-        >Jorge Baumann</a
-      >
+      <a href="https://twitter.com/Jairo251180" target="_blank">Jhon Dueñas</a>
       for
       <a href="https://platzi.com/" target="_blank">Platzi</a>
     </small>
